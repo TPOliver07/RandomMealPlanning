2f7e97f60699e555ed4bb2f7eeeb83cba308aeae
TPOliver07 <145502864+TPOliver07@users.noreply.github.com>
2023-09-19 15:59:01 -0500

    Update handler.js
    Can now open a new dialogue element, "prefDIV"

--- handler.js
+++ handler.js
@@ -1,24 +1,33 @@
 // This script is for handling preferences and swapping pages.
 
-doc = "C:\Users\Travis\Desktop\Projects\MealPlanningApp\MealPlan_v0_1_0\library\html\home.html"
+doc = "index.html"
 
 function load(type, home) {
 	if (type==="single"){
 		document.getElementById("singleDIV").style.display = "block";
 		document.getElementById("listDIV").style.display = "none";
 		document.getElementById("placeDIV").style.display = "none";
+		document.getElementById("prefDIV").style.display = "none";
 		home = "n"
 	}
 	if (type==="shoppingList"){
 		document.getElementById("singleDIV").style.display = "none";
 		document.getElementById("listDIV").style.display = "block";
 		document.getElementById("placeDIV").style.display = "none";
+		document.getElementById("prefDIV").style.display = "none";
 		home = "n"
 	}
 	if (type==="place"){
 		document.getElementById("singleDIV").style.display = "none";
 		document.getElementById("listDIV").style.display = "none";
 		document.getElementById("placeDIV").style.display = "block";
+		document.getElementById("prefDIV").style.display = "none";
 		home = "n"
 	}
+	if (type==="pref"){
+		document.getElementById("prefDIV").style.display = "block";
+		document.getElementById("singleDIV").style.display = "none";
+		document.getElementById("listDIV").style.display = "none";
+		document.getElementById("placeDIV").style.display = "none";
+		home = "n"
 }
