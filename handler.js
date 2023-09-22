b697f4a643f9a02decb81152394268d83657f785
TPOliver07 <145502864+TPOliver07@users.noreply.github.com>
2023-09-22 16:09:13 -0500

    Add files via upload
    Added two new preferences

--- handler.js
+++ handler.js
@@ -2,6 +2,11 @@
 
 doc = "index.html"
 
+var repeatB2B = true;
+var repeatAll = true;
+document.cookie = "repeatB2B = "+ repeatB2B+ ";Max-Age=220924800;path=/;SameSite=strict;";
+document.cookie = "repeatAll="+ repeatAll+ ";Max-Age=220924800;path=/;SameSite=strict;";
+
 function load(type, home) {
 	if (type==="single"){
 		document.getElementById("singleDIV").style.display = "block";
@@ -31,4 +36,29 @@ function load(type, home) {
 		document.getElementById("placeDIV").style.display = "none";
 		home = "n"
 	}
+}
+
+function changePref(pref, newVal){
+	parseCookies();
+	
+	if (pref == "repeatB2B"){
+		if (newVal == true){repeatB2B = true;}
+		else {repeatB2B = false;}
+		console.log("repeatB2B="+ repeatB2B)
+	} else if(pref == "repeatAll"){
+		if (newVal == true){
+			if(cookieList[0].split(",").length < 6 || cookieList[3].split(",").length < 6 || cookieList[6].split(",").length < 6){
+				alert("Need at least 7 items!");
+				repeatAll = false;
+				document.getElementById("repeatAllCheck").checked = false;
+			} else {
+				repeatAll = true;
+			}
+		} else {repeatAll = false;}
+		console.log("repeatAll = " + repeatAll);
+		
+	} else {console.log("No variable under that name!");}
+	
+	document.cookie = "repeatB2B="+ repeatB2B+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "repeatAll="+ repeatAll+ ";Max-Age=220924800;path=/;SameSite=strict;";
 }
--- planner.js
+++ planner.js
@@ -11,6 +11,23 @@ var dishList = new Array("Sphagetti", "Tortilla", "Paella", "Lasagna","Soup");
 var placeList = new Array("K&L", "Ichiban", "Lisa's Fifth Street Diner", "Que Pasa Tacos", "Panda Express", "Blaze Pizza", "Subway");
 // Need to get a list of all these things.
 console.log(document.cookie);
+
+
+// Call this function anytime cookies are edited to immediately put them back in order.
+function parseCookies(){
+	cookieList = document.cookie.split("; ");
+	cookieList.sort();
+	console.log(cookieList);
+	
+	for (let i = 0; i < cookieList.length; i++){
+		cookieList[i] = cookieList[i].split("=")[1];
+	}
+	
+	return cookieList;
+}
+
+parseCookies();
+
 if (!document.cookie) {
 	var proteinList = new Array("Shrimp","Tofu","Tempeh","Seitan","Mahi Mahi","Chickpeas","Impossible Meat","Veggie Burger", "Variety Fish");
 	var veggieList = new Array("Squash","Broccoli","Spinach","Asparagus","Green Beans","Carrots","Cauliflower","Avacado","Corn","Cabbage","Sweet Potato","Snap Peas");
@@ -18,25 +35,18 @@ if (!document.cookie) {
 	var dishList = new Array("Sphagetti", "Tortilla", "Paella", "Lasagna","Soup");
 	var placeList = new Array("K&L", "Ichiban", "Lisa's Fifth Street Diner", "Que Pasa Tacos", "Panda Express", "Blaze Pizza", "Subway");
 	console.log("Defined cookies");
-	document.cookie = "proteinList = "+ proteinList.toString()+ ";Max-Age=2592000000;path=/;";
-	document.cookie = "veggieList = "+ veggieList.toString()+ ";Max-Age=2592000000;path=/";
-	document.cookie = "carbList = "+ carbList.toString()+ ";Max-Age=2592000000;path=/";
-	document.cookie = "dishList = "+ dishList.toString()+ ";Max-Age=2592000000;path=/";
-	document.cookie = "placeList = "+ placeList.toString()+ ";Max-Age=2592000000;path=/";
+	document.cookie = "proteinList="+ proteinList.toString()+ ";Max-Age=2592000000;path=/;";
+	document.cookie = "veggieList="+ veggieList.toString()+ ";Max-Age=2592000000;path=/";
+	document.cookie = "carbList="+ carbList.toString()+ ";Max-Age=2592000000;path=/";
+	document.cookie = "dishList="+ dishList.toString()+ ";Max-Age=2592000000;path=/";
+	document.cookie = "placeList="+ placeList.toString()+ ";Max-Age=2592000000;path=/";
 	
 } else {
-	cookieList = document.cookie.split(";");
-	console.log(cookieList);
-	for (let i = 0; i < cookieList.length; i++) {
-		cookieList[i] = cookieList[i].split("=")[1];
-	}
-	console.log(cookieList);
-	
-	var proteinList = cookieList[0].split(",");
-	var veggieList = cookieList[1].split(",");
-	var carbList = cookieList[2].split(",");
-	var dishList = cookieList[3].split(",");
-	var placeList = cookieList[4].split(",");
+	var proteinList = cookieList[3].split(",");
+	var veggieList = cookieList[6].split(",");
+	var carbList = cookieList[0].split(",");
+	var dishList = cookieList[1].split(",");
+	var placeList = cookieList[2].split(",");
 	
 	console.log(proteinList + ";" + veggieList + ";" + carbList + ";" + dishList + ";" + placeList);
 }
@@ -62,15 +72,67 @@ var day;
 
 function generateMeal() {
 	i = 0;
+	jjj = 0;
 	proteinPlan.length = 0;
 	veggiePlan.length = 0;
 	carbPlan.length = 0;
 	
 	while (i < 7) {
 		day = String(i+1);
+		jjj = 0;
 		proteinGen = Math.floor(Math.random() * proteinList.length);
 		veggieGen = Math.floor(Math.random() * veggieList.length);
 		carbGen = Math.floor(Math.random() * carbList.length);
+		
+		if(cookieList[4] == "true"){
+			if(i>0){
+				while (jjj == 0){
+					proteinGen = Math.floor(Math.random() * proteinList.length);
+					if(proteinPlan.includes(proteinList[proteinGen])){
+						jjj=0;
+					} else { jjj = 1;
+					}
+				}
+				while (jjj == 1){
+					veggieGen = Math.floor(Math.random() * veggieList.length);
+					if(veggiePlan.includes(veggieList[veggieGen])){
+						jjj=1;
+					} else {jjj = 3;}
+				}
+					while (jjj == 2){
+					carbGen = Math.floor(Math.random() * carbList.length);
+					if(carbPlan.includes(carbList[carbGen])){
+						jjj=2;
+					} else {jjj = 3;}
+				}
+			}
+		}
+		
+		if(cookieList[5] == "true" && cookieList[4] == "false"){
+			console.log("checking for repeats");
+			if(i>0){
+				while (jjj == 0){
+					proteinGen = Math.floor(Math.random() * proteinList.length);
+					if(proteinList[proteinGen] == proteinPlan[i-1]){
+						jjj=0;
+					} else { jjj = 1;
+					console.log(proteinList[proteinGen]);
+					}
+				}
+				while (jjj == 1){
+					veggieGen = Math.floor(Math.random() * veggieList.length);
+					if(veggieList[veggieGen] == veggiePlan[i-1]){
+						jjj=1;
+					} else {jjj = 3;}
+				}
+					while (jjj == 2){
+					carbGen = Math.floor(Math.random() * carbList.length);
+					if(carbList[carbGen] == carbPlan[i-1]){
+						jjj=2;
+					} else {jjj = 3;}
+				}
+			}
+		}
 		
 		proteinPlan.push(proteinList[proteinGen]);
 		veggiePlan.push(veggieList[veggieGen]);
@@ -144,9 +206,12 @@ function savePreferences(){
 	
 	// save to cookies.
 	
-	document.cookie = "proteinList = "+ proteinList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
-	document.cookie = "veggieList = "+ veggieList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
-	document.cookie = "carbList = "+ carbList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
-	document.cookie = "dishList = "+ dishList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
-	document.cookie = "placeList = "+ placeList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "proteinList="+ proteinList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "veggieList="+ veggieList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "carbList="+ carbList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "dishList="+ dishList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	document.cookie = "placeList="+ placeList.toString()+ ";Max-Age=220924800;path=/;SameSite=strict;";
+	alert("Preferences saved successfully!");
+	
+	parseCookies();
 }
